08662eec947af08aea40c371e5dafda3631ec0ac
FabianStupperich <fabian.stupperich@volatile.de>
2025-10-23 11:41:41 +0200

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,10 +10,10 @@ export default function Hero() {
 
       <div className="relative z-10 w-full h-full flex items-end pb-20 sm:pb-24">
         <div
-          className="pl-4 sm:pl-8 md:pl-12 animate-[fadeInUp_1s_ease-out]"
+          className="pl-6 sm:pl-12 md:pl-16 lg:pl-20 animate-[fadeInUp_1s_ease-out]"
           style={{ animationDelay: '0.2s', animationFillMode: 'both' }}
         >
-          <img src="/KIVi_Antelopes_Logo.svg" alt="KIVI Antelopes Logo" className="h-24 sm:h-32 md:h-40 lg:h-48" />
+          <img src="/KIVi_Antelopes_Logo.svg" alt="KIVI Antelopes Logo" className="h-28 sm:h-36 md:h-48 lg:h-56" />
         </div>
       </div>
 
