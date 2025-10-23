ac5094a1e9ee9b0ecba7e7c91a862ca8a77b52fd
FabianStupperich <fabian.stupperich@volatile.de>
2025-10-23 16:12:56 +0200

    Updated App.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,19 @@
 export default function Hero() {
   return (
     <section id="home" className="relative h-[85vh] flex items-center justify-center overflow-hidden">
-      <div className="absolute inset-0">
+      <div className="absolute inset-0 hidden md:block">
         <div
           className="absolute inset-0 bg-[url('/wires-7997980.jpg')] bg-contain bg-center bg-no-repeat"
           style={{ backgroundSize: '100%' }}
         ></div>
       </div>
 
-      <div className="relative z-10 w-full h-full flex items-end" style={{ paddingBottom: '5vh' }}>
+      <div className="relative z-10 w-full h-full flex items-center md:items-end justify-center md:justify-start" style={{ paddingBottom: '0', paddingLeft: '0' }}>
         <div
-          className="animate-[fadeInUp_1s_ease-out]"
+          className="animate-[fadeInUp_1s_ease-out] md:pl-[4vw] md:pb-[5vh]"
           style={{
             animationDelay: '0.2s',
-            animationFillMode: 'both',
-            paddingLeft: '4vw'
+            animationFillMode: 'both'
           }}
         >
           <img
