6b15dc5f34cf1a3056a70805d78ee2f1e109a456
FabianStupperich <fabian.stupperich@volatile.de>
2025-11-12 15:54:54 +0100

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -44,7 +44,7 @@ export default function Navbar() {
   const isKonsortium = location.pathname === '/konsortium';
 
   return (
-    <nav className={`fixed top-0 left-0 right-0 z-50 transition-colors duration-300 bg-black ${isImpressum || isDatenschutz || isNewsPage || isPublikationen || isKonsortium || isScrolled ? 'sm:bg-white' : 'sm:bg-transparent'}`}>
+    <nav className={`fixed top-0 left-0 right-0 z-50 transition-colors duration-300 bg-black ${isImpressum || isDatenschutz || isNewsPage || isPublikationen || isKonsortium || isScrolled ? 'sm:bg-black' : 'sm:bg-transparent'}`}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 py-3">
         <div className="flex items-center justify-between">
           <button onClick={() => navigate('/')} className="flex items-center space-x-2">
