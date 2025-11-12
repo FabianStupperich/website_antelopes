f176dfb89ad9332014bf041e77273543826e1c82
FabianStupperich <fabian.stupperich@volatile.de>
2025-11-12 16:03:42 +0100

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
       <div className="absolute inset-0 hidden md:block">
         <div
           className="absolute inset-0 bg-[url('/hero.png')] bg-contain bg-center bg-no-repeat"
-          style={{ backgroundSize: '100%', filter: 'brightness(0.75)' }}
+          style={{ backgroundSize: '100%', filter: 'brightness(0.5)' }}
         ></div>
       </div>
 
