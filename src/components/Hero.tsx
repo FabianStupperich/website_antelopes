a73050531ffa517f31273e2e52bdd933cc0fc2e6
FabianStupperich <fabian.stupperich@volatile.de>
2025-11-13 21:56:28 +0100

    Updated wind_offshore.png

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <section id="home" className="relative h-[85vh] flex items-center justify-center overflow-hidden">
-      <div className="absolute inset-0 hidden lg:block">
+      <div className="absolute inset-0 hidden md:block">
         <div
           className="absolute inset-0 bg-[url('/hero.png')] bg-contain bg-center bg-no-repeat"
           style={{ backgroundSize: '100%', filter: 'brightness(0.75)' }}
